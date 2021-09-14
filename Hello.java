60e674a300fc82b84411a14e0b666c43bab0a2aa
Pradana Setialana <pradana.setialana@gmail.com>
2021-09-14 13:08:17 +0700

    menambahkan komentar berupa nama dan nim

--- Hello.java
+++ Hello.java
@@ -1,5 +1,10 @@
+/*
+NIM : 123456
+Nama : Pradana
+*/
 public class Hello {
     public static void main(String[] args) {
+        // untuk menampilkan teks
         System.out.println("Hello world");
     }
 }
