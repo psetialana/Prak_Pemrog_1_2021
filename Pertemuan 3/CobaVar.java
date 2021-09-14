846d54f5fbbba56d9a4b0d5376666a12a2fabe89
Pradana Setialana <pradana.setialana@gmail.com>
2021-09-14 13:38:22 +0700

    belajar variabel dalam java

--- Pertemuan 3/CobaVar.java
+++ Pertemuan 3/CobaVar.java
@@ -1,3 +1,10 @@
 public class CobaVar {
-    
+    public static void main(String[] args) {
+        String namaDepan = "Pradana";
+        int umur = 29;
+        double tinggiBadan = 155;
+        System.out.println("Nama Saya " + namaDepan);
+        System.out.println("Umur saya " + umur);
+        System.out.println("Tinggi badan saya " + tinggiBadan);
+    }
 }
