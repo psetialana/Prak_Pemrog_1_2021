80f277cc0616e299029d5ba8ca011b44b472d867
Pradana Setialana <pradana.setialana@gmail.com>
2021-10-26 13:43:09 +0700

    tambah proyek

--- Pertemuan 9/TugasKalkulator.java
+++ Pertemuan 9/TugasKalkulator.java
@@ -1,10 +1,19 @@
+import java.util.Scanner;
 public class TugasKalkulator {
     
     public static void main(String[] args) {
-        System.out.println("Masukan nilai A = ");
-        System.out.println("Masukan nilai B = ");
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Masukan nilai A = ");
+        int nilaiA = scan.nextInt();
+        System.out.print("Masukan nilai B = ");
+        int nilaiB = scan.nextInt();
 
         int hasil = penjumlahan(nilaiA, nilaiB);
-        System.out.println("Nilai A + B = " + hasil)
+        System.out.println("Nilai A + B = " + hasil);
+    }
+
+    public static int penjumlahan(int a, int b) {
+        int hasil = a + b;
+        return hasil;
     }
 }
---
+++ Pertemuan 9/belajarfunction.py
@@ -0,0 +1,14 @@
+def coba():
+    print("coba function")
+
+def penjumlahan(a, b):
+    hasil = a + b
+    print("hasil penjumlahan = " + str(hasil))
+
+def perkalian(a, b):
+    hasil = a*b
+    return hasil
+
+coba()
+penjumlahan(1, 6)
+print(perkalian(5, 6))
